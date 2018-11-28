d15aa483af596b5e90ae8913ed900ddf5442d289
Codey Worley <codeyworley@gmail.com>
2018-11-28 12:12:31 -0700

    trigger travis

--- routes/notes.js
+++ routes/notes.js
@@ -1,103 +1,65 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const { MONGODB_URI } = require('../config');
-
 const Note = require('../models/note');
-
 const express = require('express');
-
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
-  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-    .then(() => {
-      return Note.find().sort({ updatedAt: 'desc' });
-    })
+  const searchTerm = req.query.searchTerm;
+  let filter = {};
+  if (searchTerm) {
+    filter.title = { $regex: searchTerm, $options: 'i' };
+    const re = new RegExp(searchTerm, 'i');
+    filter.$or = [{ 'title': re }, { 'content': re }];
+  }
+  Note.find(filter)
+    .sort({ updatedAt: 'desc' })
     .then(results => {
       res.json(results);
     })
-    .then(() => {
-      return mongoose.disconnect()
-    })
-    .catch(err => {
-      return next(err);
-    });
+    .catch(err =>  next(err));
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-    .then(() => {
-        return Note.findById(id);
-    })
+  Note.findById(id)
     .then(results => {
       res.json(results);
     })
-    .then(() => {
-      return mongoose.disconnect()
-    })
-    .catch(err => {
-      return next(err);
-    });
+    .catch(err => next(err));
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
   const note = req.body;
-  mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-    .then(() => {
-        return Note.create(note);
-    })
+  Note.create(note)
     .then(results => {
       res.location('path/to/new/document').status(201).json(results);
     })
-    .then(() => {
-      return mongoose.disconnect()
-    })
-    .catch(err => {
-      return next(err);
-    });
+    .catch(err => next(err));
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
   const id = req.params.id;
   const update = req.body;
-  mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-    .then(() => {
-        return Note.findByIdAndUpdate(id, {$set: update}, {new: true});
-    })
+  Note.findByIdAndUpdate(id, {$set: update}, {new: true})
     .then(results => {
       res.json(results);
     })
-    .then(() => {
-      return mongoose.disconnect()
-    })
-    .catch(err => {
-      return next(err);
-    });
+    .catch(err => next(err));
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-  mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-  .then(() => {
-      return Note.findByIdAndDelete(id);
-  })
-  .then(results => {
-    res.status(204).end();
-  })
-  .then(() => {
-    return mongoose.disconnect()
-  })
-  .catch(err => {
-    return next(err);
-  });
-  
+  Note.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
