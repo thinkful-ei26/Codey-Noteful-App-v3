772f99ac2054e96122875fc6e6ad39161e2bc2f3
Codey Worley <codeyworley@gmail.com>
2018-11-28 13:12:35 -0700

    routed to mlab db

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
-
+//'mongodb://localhost:27017/noteful'
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://admin:password1@ds119394.mlab.com:19394/notes',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://admin:password1@ds119394.mlab.com:19394/notes'
 };
