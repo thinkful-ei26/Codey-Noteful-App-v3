9ad6feac05ee66414000209b4a698d0768f1929e
CodeyWorley <codeyworley@gmail.com>
2018-12-02 15:24:27 -0700

    hid keys require and replaced with heroku globals

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 //'mongodb://localhost:27017/noteful' for local env
-const keys = require('./keys');
+//const keys = require('./keys'); **commented out for travisci**
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || keys.MONGO_DB || MONGODB_URI,
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || keys.MONGO_DB_DEV || MONGODB_URI_DEV
+  MONGODB_URI: process.env.MONGODB_URI || /** keys.MONGO_DB ||*/ MONGODB_URI,
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || /** keys.MONGO_DB_DEV ||*/ MONGODB_URI_DEV
 };
