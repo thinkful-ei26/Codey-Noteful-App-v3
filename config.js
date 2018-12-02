91b4a7174542858b69b2ed3200fde686b6219853
CodeyWorley <codeyworley@gmail.com>
2018-12-02 15:10:18 -0700

    changed dev config route to static

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ const keys = require('./keys');
 module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || keys.MONGO_DB,
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || keys.MONGO_DB_DEV
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://admin:password1@ds031792.mlab.com:31792/noteful-dev'
 };
